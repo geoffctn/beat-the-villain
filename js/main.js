1937151e511051718ae817cc11849fa0dbe3d67b
Lykon <geoffrey.couten@gmail.com>
2015-06-04 20:59:48 +0200

    IMG, HTML, CSS, JS improvements

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 /*************** VILLAINS CARACTERISTICS ****************/
 
 // Variables
-var blackMask = {name: "Black Mask", maxlife: 500, life: 500, reward: 25, cover: "http://lykon.github.io/beat-the-villain/img/blackmask1.jpg"},
-    deadshot = {name: "Deadshot", maxlife: 8000, life: 8000, reward: 400, cover: "http://lykon.github.io/beat-the-villain/img/deadshot1.jpg"},
-    poisonIvy = {name: "Poison Ivy", maxlife: 15000, life: 15000, reward: 1300, cover: "http://lykon.github.io/beat-the-villain/img/poisonivy1.jpg"},
-    killerCroc = {name: "Killer Croc", maxlife: 38000, life: 38000, reward: 3200, cover: "http://lykon.github.io/beat-the-villain/img/killercroc1.jpg"},
-    harleyQuinn = {name: "Harley Quinn", maxlife: 100000, life: 100000, reward: 14000, cover: "http://lykon.github.io/beat-the-villain/img/harleyquinn1.jpg"},
-    hugoStrange = {name: "Hugo Strange", maxlife: 350000, life: 350000, reward: 50000, cover: "http://lykon.github.io/beat-the-villain/img/hugostrange1.jpg"},
-    mrFreeze = {name: "Mr Freeze", maxlife: 800000, life: 800000, reward: 250000, cover: "http://lykon.github.io/beat-the-villain/img/mrfreeze1.jpg"},
-    pengouin = {name: "The Penguin", maxlife: 200000, life: 200000, reward: 900000, cover: "http://lykon.github.io/beat-the-villain/img/penguin1.jpg"},
-    deathstroke = {name: "Deathstroke", maxlife: 600000, life: 600000, reward: 2500000, cover: "http://lykon.github.io/beat-the-villain/img/deathstroke1.jpg"},
-    bane = {name: "Bane", maxlife: 2000000, life: 2000000, reward: 12000000, cover: "http://lykon.github.io/beat-the-villain/img/bane1.jpg"},
-    rasAlGhul = {name: "Ra's Al Ghul", maxlife: 50000000, life: 50000000, reward: 50000000, cover: "http://lykon.github.io/beat-the-villain/img/rasalghul1.jpg"},
-    joker = {name: "The Joker", maxlife: 999999999, life: 999999999, reward: 999999999, cover: "http://lykon.github.io/beat-the-villain/img/joker1.jpg"};
+var blackMask = {name: "Black Mask",color: "#ffffff", maxlife: 500, life: 500, reward: 25, cover: "img/blackmask1.jpg"},
+    deadshot = {name: "Deadshot",color: "#af5838", maxlife: 8000, life: 8000, reward: 400, cover: "img/deadshot1.jpg"},
+    poisonIvy = {name: "Poison Ivy",color: "#7ad321", maxlife: 15000, life: 15000, reward: 1300, cover: "img/poisonivy1.jpg"},
+    killerCroc = {name: "Killer Croc",color: "#306d3c", maxlife: 38000, life: 38000, reward: 3200, cover: "img/killercroc1.jpg"},
+    harleyQuinn = {name: "Harley Quinn",color: "#ff3333", maxlife: 100000, life: 100000, reward: 14000, cover: "img/harleyquinn1.jpg"},
+    hugoStrange = {name: "Hugo Strange",color: "#d1d1d1", maxlife: 350000, life: 350000, reward: 50000, cover: "img/hugostrange1.jpg"},
+    mrFreeze = {name: "Mr Freeze",color: "#5dd3ff", maxlife: 800000, life: 800000, reward: 250000, cover: "img/mrfreeze1.jpg"},
+    pengouin = {name: "The Penguin",color: "#6f6f6f", maxlife: 200000, life: 200000, reward: 900000, cover: "img/penguin1.jpg"},
+    deathstroke = {name: "Deathstroke",color: "#f0db36", maxlife: 600000, life: 600000, reward: 2500000, cover: "img/deathstroke1.jpg"},
+    bane = {name: "Bane",color: "#47f413", maxlife: 2000000, life: 2000000, reward: 12000000, cover: "img/bane1.jpg"},
+    rasAlGhul = {name: "Ra's Al Ghul",color: "#7cbc3b", maxlife: 50000000, life: 50000000, reward: 50000000, cover: "img/rasalghul1.jpg"},
+    joker = {name: "The Joker",color: "#ff0000", maxlife: 999999999, life: 999999999, reward: 999999999, cover: "img/joker1.jpg"};
 
 var villainOn = blackMask;
 var villainOnId = 0;
@@ -321,7 +321,8 @@ function updateData(){
     $(".villain_maxlife_stats").html(villainOn.maxlife).digits();
     $(".villain_name").html(villainOn.name);
     villainLifePercentage();
-    $(".villain_pic").css({"background" : "url(" + villainOn.cover + ") no-repeat center center fixed", "-webkit-background-size" : "cover", "-moz-background-size" : "cover", "-o-background-size" : "cover", "background-size" : "cover"});
+    $("#villain_avatar").attr("src",villainOn.cover);
+    $(".villain_name").css("color", villainOn.color);
     
     // update heroes
     $("#heroes_DPS").html(heroesTeamDps).digits();
